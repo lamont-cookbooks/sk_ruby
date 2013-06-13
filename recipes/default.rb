08dc1395673b894868b7bdbdab1661f3cd604f9b
Lamont Granquist <lamont@scriptkiddie.org>
2013-06-13 15:27:04 -0700

    update ruby to 2.0.0-p195

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,23 +9,30 @@ include_recipe "sk_ruby::ubuntu"
 
 chef_gem "fpm"
 
+my_gems = [
+  "bundler",
+  "rake",
+  "fpm",
+  "berkshelf",
+]
+
 sk_ruby "1.9.3-p327" do
   rubygems "1.8.24"
-  gems [ "bundler", "rake", "fpm" ]
+  gems my_gems
   cache_uri_base "http://s3.scriptkiddie.org/ruby"
 end
 
 sk_ruby "1.9.3-p429" do
   rubygems "2.0.3"
-  gems [ "bundler", "rake", "fpm" ]
+  gems my_gems
   cache_uri_base "http://s3.scriptkiddie.org/ruby"
 end
 
 sk_ruby "2.0.0-p195" do
   rubygems "2.0.3"
-  gems [ "bundler", "rake", "fpm" ]
+  gems my_gems
   cache_uri_base "http://s3.scriptkiddie.org/ruby"
 end
 
-sk_ruby_symlinks "/opt/ruby-1.9.3-p327/bin"
+sk_ruby_symlinks "/opt/ruby-2.0.0-p195/bin"
 
