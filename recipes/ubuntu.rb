a81a5a036377bbcc75aec6343658fd8e652767f9
Lamont Granquist <lamont@scriptkiddie.org>
2013-06-22 15:20:04 -0700

    add 1.9.3 to remove, add libxml2 and libxslt deps

--- recipes/ubuntu.rb
+++ recipes/ubuntu.rb
@@ -1,13 +1,13 @@
 
 # remove old rubies
-%w{ ruby ruby1.8-dev ruby1.9.1 ruby1.9.1-dev libruby1.9.1 libruby-extras libruby1.8-extras rubygems rubygems1.8 }.each do |pkg|
+%w{ ruby ruby1.8-dev ruby1.9.1 ruby1.9.3 ruby1.9.1-dev libruby1.9.1 libruby-extras libruby1.8-extras rubygems rubygems1.8 }.each do |pkg|
   package pkg do
     action :remove
   end
 end
 
 # install packages necessary to build
-%w{ wget build-essential zlib1g-dev libreadline6-dev libssl-dev libyaml-dev }.each do |pkg|
+%w{ wget build-essential zlib1g-dev libreadline6-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev }.each do |pkg|
   package pkg
 end
 
