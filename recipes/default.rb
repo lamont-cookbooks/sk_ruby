929851de7a3466e74614e1e249457a2150b2b40e
Lamont Granquist <lamont@scriptkiddie.org>
2013-08-15 12:59:37 -0700

    removing old 1.9 versions and bumping ruby 2.0

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,26 +21,26 @@ my_gems = [
   "puma",
 ]
 
-sk_ruby "1.9.3-p327" do
-  rubygems "1.8.24"
-  gems my_gems
-  cache_uri_base "http://s3.scriptkiddie.org/ruby"
-  pkg_version "0.0.2"
-end
+#sk_ruby "1.9.3-p327" do
+#  rubygems "1.8.24"
+#  gems my_gems
+#  cache_uri_base "http://s3.scriptkiddie.org/ruby"
+#  pkg_version "0.0.2"
+#end
+#
+#sk_ruby "1.9.3-p429" do
+#  rubygems "2.0.3"
+#  gems my_gems
+#  cache_uri_base "http://s3.scriptkiddie.org/ruby"
+#  pkg_version "0.0.2"
+#end
 
-sk_ruby "1.9.3-p429" do
+sk_ruby "2.0.0-p247" do
   rubygems "2.0.3"
   gems my_gems
   cache_uri_base "http://s3.scriptkiddie.org/ruby"
   pkg_version "0.0.2"
 end
 
-sk_ruby "2.0.0-p195" do
-  rubygems "2.0.3"
-  gems my_gems
-  cache_uri_base "http://s3.scriptkiddie.org/ruby"
-  pkg_version "0.0.2"
-end
-
-sk_ruby_symlinks "/opt/ruby-2.0.0-p195/bin"
+sk_ruby_symlinks "/opt/ruby-2.0.0-p247/bin"
 
