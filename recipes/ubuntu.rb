038dcad4f5cb166593c2760bf36f91a510956a5b
Lamont Granquist <lamont@scriptkiddie.org>
2013-10-25 15:40:42 -0700

    properly include dep on apt

--- recipes/ubuntu.rb
+++ recipes/ubuntu.rb
@@ -1,4 +1,6 @@
 
+include_recipe "apt"
+
 # remove old rubies
 %w{ ruby ruby1.8-dev ruby1.9.1 ruby1.9.3 ruby1.9.1-dev libruby1.9.1 libruby-extras libruby1.8-extras rubygems rubygems1.8 }.each do |pkg|
   package pkg do
