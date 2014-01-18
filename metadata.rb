369113a5e515076c4a8b9d76dbb1f1cc826f3ee5
Lamont Granquist <lamont@scriptkiddie.org>
2014-01-18 14:41:18 -0800

    fix description

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 maintainer       "Lamont Granquist"
 maintainer_email "lamont@scriptkiddie.org"
 license          "Apache 2.0"
-description      "LWRPs for installing source-built rubies"
-long_description "LWRPs for installing source-built rubies"
+description      "LWRPs for installing source-built rubies.  Automatically uploads built s3 binaries.  Subsequent runs will download instead of rebuilding."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.1"
 name             "sk_ruby"
 
