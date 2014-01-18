814256316060096c936a9b75b28220f043a6676f
Lamont Granquist <lamont@scriptkiddie.org>
2014-01-18 11:40:57 -0800

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "lamont@scriptkiddie.org"
 license          "Apache 2.0"
 description      "SCRIPTKIDDIES DONT USE DESCRIPTIONS"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.7"
+version          "0.0.8"
 name             "sk_ruby"
 
 depends "build-essential"
