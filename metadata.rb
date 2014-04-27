fb5f6caa02c983d13e620978c3ab10cd42f8a785
Lamont Granquist <lamont@scriptkiddie.org>
2014-04-27 15:33:01 -0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "lamont@scriptkiddie.org"
 license          "Apache 2.0"
 description      "LWRPs for installing source-built rubies.  Automatically uploads built s3 binaries.  Subsequent runs will download instead of rebuilding."
 long_description "LWRPs for installing source-built rubies.  Automatically uploads built s3 binaries.  Subsequent runs will download instead of rebuilding."
-version          "2.0.4"
+version          "2.0.5"
 name             "sk_ruby"
 
 depends "build-essential", '>= 2.0.0'
