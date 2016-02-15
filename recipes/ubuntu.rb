6e5636652df6d7d54b58f6d8ce7edf98a87950fa
Lamont Granquist <lamont@scriptkiddie.org>
2016-02-15 12:45:56 -0800

    busted for now

--- recipes/ubuntu.rb
+++ recipes/ubuntu.rb
@@ -2,11 +2,11 @@
 include_recipe "apt"
 
 # remove old rubies
-pkgs_remove = %w{ ruby ruby1.9.1 ruby1.9.3 ruby1.9.1-dev libruby1.9.1 libruby-extras libruby1.8-extras }
-
-if node['platform'] == 'ubuntu' && node['platform_version'].to_f < 14.04
-  pkgs_remove += %w{ ruby1.8-dev rubygems rubygems1.8 }
-end
+#pkgs_remove = %w{ ruby ruby1.9.1 ruby1.9.3 ruby1.9.1-dev libruby1.9.1 libruby-extras libruby1.8-extras }
+#
+#if node['platform'] == 'ubuntu' && node['platform_version'].to_f < 14.04
+#  pkgs_remove += %w{ ruby1.8-dev rubygems rubygems1.8 }
+#end
 
 multipackage pkgs_remove do
   action :remove
